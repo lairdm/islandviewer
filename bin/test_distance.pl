3784b740104fbb7df2a89f0aec4ddcf29cab2cb0
lairdm <lairdm@sfu.ca>
2013-10-07 16:31:19 -0700

    Test the distance module and a sample config file

---
+++ bin/test_distance.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use Cwd qw(abs_path getcwd);
+use Getopt::Long;
+
+BEGIN{
+# Find absolute path of script
+my ($path) = abs_path($0) =~ /^(.+)\//;
+chdir($path);
+sub mypath { return $path; }
+};
+
+use lib "../lib";
+use lib "/home/lairdm/libs";
+use Islandviewer;
+use Islandviewer::Config;
+use Islandviewer::Distance;
+
+MAIN: {
+    my $cfname;
+    my $res = GetOptions("config=s" => \$cfname
+    );
+
+    die "Error, no config file given"
+      unless($cfname);
+
+    my $Islandviewer = Islandviewer->new({cfg_file => $cfname });
+
+    my $cfg = Islandviewer::Config->config;
+
+    my $dist_obj = Islandviewer::Distance->new({scheduler => 'Islandviewer::MetaScheduler', workdir => $cfg->{workdir}, num_jobs => 8 });
+
+#    $dist_obj->calculate_all();
+    $dist_obj->submit_sets();
+
+
+}
